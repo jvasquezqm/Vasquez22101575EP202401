d3a1d869636a01bd53a3901422face578b9db140
Jonathan <jvasquezqm@gmail.com>
2024-05-19 12:07:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/vasquez22101575ep202401/InicioActivity.kt
@@ -0,0 +1,27 @@
+package com.example.vasquez22101575ep202401
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_inicio)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnGetStart= findViewById<Button>(R.id.btnGetStart)
+        btnGetStart.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
